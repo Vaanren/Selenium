214f34da3b8e56ce551dab51c211c068e1baf93d
Vaanren <vaanren@mail.ru>
2020-02-20 23:11:56 +0300

    Start

---
+++ selenium-example/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("selenium-example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("selenium-example")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4e530ca-2a82-4f78-8149-cc4d6d3bb1b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ selenium-example/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium_example
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [TestInitialize]
+        public void Star()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+        // это тест.
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Url = "http://www.google.com/";
+            driver.FindElement(By.XPath("//*[@class = 'gLFyf gsfi']")).SendKeys("webdriver");
+            driver.FindElement(By.Name("btnK")).SendKeys(Keys.Enter);
+        }
+
+        // разрушение объекта драйвера после окончание теста.
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
