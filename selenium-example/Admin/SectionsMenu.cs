7db28c7ffde34f2ae8c9a385409a060dcbc7ab48
Vaanren <vaanren@mail.ru>
2020-02-25 03:49:47 +0300

    Проверка отображения всех пунктов меню админки

--- selenium-example/Admin/SectionsMenu.cs
+++ selenium-example/Admin/SectionsMenu.cs
@@ -1,22 +1,21 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
+using OpenQA.Selenium.Support.UI;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace selenium_example.Tests
+
+namespace selenium_example.Admin
 {
     /*
-     * Тест
+     * Тест отображения всех пунктов меню
     1. Входит в панель администратора http://localhost/litecart/admin
     2. Прокликивает последовательно все пункты меню слева, включая вложенные пункты
     3. для каждой страницы проверяет наличие заголовка(то есть элемента с тегом h1)
     */
     [TestClass]
-    public class AdminSections : BaseSteps
+    public class SectionsMenu : BaseSteps
     {
+
         [TestMethod]
         public void TestMethod1()
         {
@@ -29,25 +28,25 @@ namespace selenium_example.Tests
 
             Browser.FindElement(By.XPath("//button[@name='login']")).Click();
 
-            //Корневой бокс пунктов меню
-            //Является надежной платформой, все пункты меню - его прямые потомки
-            IWebElement box = Browser.FindElement(By.XPath("//ul[@id='box-apps-menu']"));
-
-            //Находим все пункты меню (являются списками), складываем в массив, возвращаем длинну массива.
+            //Находим все пункты меню (являются списками) опираясь на родительский узел, складываем в массив, возвращаем его длину.
             //Так мы сделаем тест более устойчивым к будущим изменениям, т.к. пункты могут добавляться и исчезать.
-            
-            int sections = box.FindElements(By.XPath("./child::li")).ToArray().Length;
+            int sections = Browser.FindElements(By.XPath("//ul[@id='box-apps-menu']/child::li")).ToArray().Length;
 
             //Кликаем по очереди на пункты меню, собирая количество его подпунктов, чтобы прокликать их тоже
-            for(int i = 1; i <= sections;  i++)
+            // Ждем отображение заголовка
+            for (int i = 1; i <= sections; i++)
             {
                 Browser.FindElement(By.XPath($"//ul[@id='box-apps-menu']/child::li[{i}]")).Click();
-                
+
+                Wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("h1")));
+
                 int subSections = Browser.FindElements(By.XPath($"//ul[@id='box-apps-menu']/child::li[{i}]/descendant::li")).ToArray().Length;
-                
+
                 for(int x = 1; x <= subSections; x++)
                 {
                     Browser.FindElement(By.XPath($"//ul[@id='box-apps-menu']/child::li[{i}]/descendant::li[{x}]")).Click();
+
+                    Wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("h1")));
                 }
             }
         }
